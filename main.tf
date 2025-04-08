6c0e7005a380624ed775d7852a900a65472c951f
zycoding <zycoding@gmail.com>
2025-04-08 13:05:30 -0400

    Add depends_on to aws_s3_bucket_policy resource

--- main.tf
+++ main.tf
@@ -48,23 +48,25 @@ resource "aws_s3_bucket_acl" "bucket" {
 resource "aws_s3_bucket_policy" "policy" {
   bucket = aws_s3_bucket.bucket.id
   policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "PublicReadGetObject",
-            "Effect": "Allow",
-            "Principal": "*",
-            "Action": [
-                "s3:GetObject"
-            ],
-            "Resource": [
-                "arn:aws:s3:::${aws_s3_bucket.bucket.id}/*"
-            ]
-        }
-    ]
-}
-EOF
+  {
+      "Version": "2012-10-17",
+      "Statement": [
+          {
+              "Sid": "PublicReadGetObject",
+              "Effect": "Allow",
+              "Principal": "*",
+              "Action": [
+                  "s3:GetObject"
+              ],
+              "Resource": [
+                  "arn:aws:s3:::${aws_s3_bucket.bucket.id}/*"
+              ]
+          }
+      ]
+  }
+  EOF
+
+  depends_on = [aws_s3_bucket_public_access_block.bucket]
 }
 
 resource "aws_s3_object" "webapp" {
